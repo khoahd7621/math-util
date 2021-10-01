2fa15adcd5fa7a090696995a2283a9aa190d3503
khoahd7621 <hoangdangkhoa7621@gmail.com>
2021-10-01 15:43:57 +0700

    add recursion to demonstrate regression testing. GREEN 4EVER again

--- src/com/khoahd7621/mathutil/core/MathUtil.java
+++ src/com/khoahd7621/mathutil/core/MathUtil.java
@@ -10,21 +10,40 @@ public class MathUtil {
     //0! = 1! = 1
     //21! tràn kiểu long
     //<0! ko tính được, ko có giai thừa cho số âm
+//    public static long getFactorial(int n) {
+//        
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be between 0..");
+//        
+//        if (n == 0 || n == 1)
+//            return 1;
+//        
+//        long product = 1;
+//        
+//        for (int i = 1; i <= n; i++)
+//            product *= i;
+//        
+//        return product;
+//        
+//    }
+    
+    //Recursion - đệ quy, gọi lại chính mình với qui mô khác
+    //Búp bê người Nga
+    //5! = 1.2.3.4.5
+    //5! = 5 * 4!
+    //4! = 4 * 3!
+    //3! = 3 * 2!
+    //2! = 2 * 1!
+    //1! = 1 -> điểm dừng
+    //n! = n * (n - 1)!
     public static long getFactorial(int n) {
-        
         if (n < 0 || n > 20)
-            throw new IllegalArgumentException("n must be between 0..");
+            throw new IllegalArgumentException("Invalid arg.");
         
         if (n == 0 || n == 1)
-            return 1;
+            return 1; // điểm dừng
         
-        long product = 1;
-        
-        for (int i = 1; i <= n; i++)
-            product *= i;
-        
-        return product;
-        
+        return n * getFactorial(n - 1); // đệ qui
     }
 
 }
